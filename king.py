fdb0df67de88aea0e3e4aa2f8e9fc86811ce80a4
Gabriel Brandão <45377520+Gabrielbrandao27@users.noreply.github.com>
2021-03-17 22:41:22 -0300

    Update king.py

--- king.py
+++ king.py
@@ -6,9 +6,81 @@ class King(Piece):
     
     # position eh uma tupla coordenada (linha,coluna)
     def move(self, position, board):
-        # implementar movimentacao
-        pass
+        possible_moves = []
+
+        x = position[0]
+        y = position[1]
+
+        if board.board_state[x][y] != None: return possible_moves # tem uma peca no caminho
+
+        possible_moves.extend(
+            [position[0] + 1, position[1]], 
+            [position[0] - 1, position[1]], 
+            [position[0], position[1] + 1], 
+            [position[0], position[1] - 1],
+            [position[0] + 1, position[1] + 1],
+            [position[0] - 1, position[1] + 1],
+            [position[0] + 1, position[1] - 1],
+            [position[0] - 1, position[1] - 1]
+            )
+
+        return possible_moves
     
     def attack(self, position, board):
-        # implementar ataque
-        pass
+        possible_attacks = []
+
+        x = position[0]
+        y = position[0]
+        if y < 0 or y > 7: return possible_attacks
+        if(board.board_state[x][y] != None and board.board_state[x][y].color != self.color):
+            x = (position[0] + 1)
+            y = position[0]
+            if y < 0 or y > 7: return possible_attacks
+            if(board.board_state[x][y] != None and board.board_state[x][y].color != self.color):
+                possible_attacks.append((x,y))
+
+            x = (position[0] - 1)
+            y = position[1]
+            if y < 0 or y > 7: return possible_attacks
+            if(board.board_state[x][y] != None and board.board_state[x][y].color != self.color):
+                possible_attacks.append((x,y))
+
+            x = position[0]
+            y = (position[1] + 1)
+            if y < 0 or y > 7: return possible_attacks
+            if(board.board_state[x][y] != None and board.board_state[x][y].color != self.color):
+                possible_attacks.append((x,y))
+
+            x = position[0]
+            y = (position[1] - 1)
+            if y < 0 or y > 7: return possible_attacks
+            if(board.board_state[x][y] != None and board.board_state[x][y].color != self.color):
+                possible_attacks.append((x,y))
+
+            x = (position[0] + 1)
+            y = (position[1] + 1)
+            if y < 0 or y > 7: return possible_attacks
+            if(board.board_state[x][y] != None and board.board_state[x][y].color != self.color):
+                possible_attacks.append((x,y))
+
+            x = (position[0] - 1)
+            y = (position[1] + 1)
+            if y < 0 or y > 7: return possible_attacks
+            if(board.board_state[x][y] != None and board.board_state[x][y].color != self.color):
+                possible_attacks.append((x,y))
+
+            x = (position[0] - 1)
+            y = (position[1] - 1)
+            if y < 0 or y > 7: return possible_attacks
+            if(board.board_state[x][y] != None and board.board_state[x][y].color != self.color):
+                possible_attacks.append((x,y))
+                
+            x = (position[0] + 1)
+            y = (position[1] - 1)
+            if y < 0 or y > 7: return possible_attacks
+            if(board.board_state[x][y] != None and board.board_state[x][y].color != self.color):
+                possible_attacks.append((x,y))
+            
+        
+
+        return possible_attacks
