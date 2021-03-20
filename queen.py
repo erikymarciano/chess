dd116df2615f338fa550a27deec441581f9b3c01
Eriky Nunes <erikymarciano@id.uff.br>
2021-03-20 12:12:18 -0300

    adicionado movimentação e ataque da rainha

--- queen.py
+++ queen.py
@@ -6,9 +6,36 @@ class Queen(Piece):
     
     # position eh uma tupla coordenada (linha,coluna)
     def move(self, position, board):
-        # implementar movimentacao
-        pass
+        possible_moves = []
+        directions = [(1,0),(0,1),(1,1),(-1,0),(0,-1),(-1,-1),(1,-1),(-1,1)]
+
+        for direction in directions:
+            aux = 1
+            x = position[0] + aux*direction[0]
+            y = position[1] + aux*direction[1]
+            while  0 <= x <= 7 and 0 <= y <= 7 and board.board_state[x][y] == None:
+                possible_moves.append((x,y))
+                aux += 1
+                x = position[0] + aux*direction[0]
+                y = position[1] + aux*direction[1]
+
+        return possible_moves
     
     def attack(self, position, board):
-        # implementar ataque
-        pass
+        possible_attacks = []
+        directions = [(1,0),(0,1),(1,1),(-1,0),(0,-1),(-1,-1),(1,-1),(-1,1)]
+
+        for direction in directions:            
+            aux = 1
+            x = position[0] + aux*direction[0]
+            y = position[1] + aux*direction[1]
+            while  0 <= x <= 7 and 0 <= y <= 7 :
+                if(board.board_state[x][y] != None and board.board_state[x][y].color == self.color):                    
+                    break
+                if(board.board_state[x][y] != None and board.board_state[x][y].color != self.color):
+                    possible_attacks.append((x,y))
+                aux += 1
+                x = position[0] + aux*direction[0]
+                y = position[1] + aux*direction[1]
+
+        return possible_attacks
